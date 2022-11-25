9e5c425dd95ed837ffa1d24afcded8adc448212e
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-11-25 17:00:34 +0200

    initial

---
+++ src/main/java/com/middle/htmlparser/Parser.java
@@ -0,0 +1,5 @@
+package com.middle.htmlparser;
+
+public class Parser {
+
+}
---
+++ src/test/java/com/middle/htmlparser/HtmlparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middle.htmlparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
