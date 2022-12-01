31e8d098b6dcd083e30a639bb80f8deb38accd1d
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-12-01 22:51:14 +0200

    add config for debug

---
+++ src/main/java/com/middle/htmlparser/PrivateSpy/PrivateSpyConfig.java
@@ -0,0 +1,44 @@
+package com.middle.htmlparser.PrivateSpy;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+@Configuration
+public class PrivateSpyConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(PrivateSpyRepository repository){
+        return args -> {
+            PrivateSpy spy1 = new PrivateSpy(
+                    1,
+                    "ucu.edu.ua",
+                    "Ukrainian Catholic University",
+                    "Kozelnytska x",
+                    "twitter.com",
+                    "facbook.com",
+                    "logo.png",
+                    "icon.png",
+                    "empoyees500"
+            );
+            PrivateSpy spy2 = new PrivateSpy(
+                    2,
+                    "kpi.kpi.ua",
+                    "KPI",
+                    "Kyiv",
+                    "twitterKpi.com",
+                    "facbookKpi.com",
+                    "logoKpi.png",
+                    "iconKpi.png",
+                    "500"
+            );
+
+            repository.saveAll(
+                    List.of(spy1, spy2)
+            );
+        };
+    }
+}
