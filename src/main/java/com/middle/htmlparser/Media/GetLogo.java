b132284c985507eb46d3d6efcc3322e81fd33f91
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-12-02 22:55:41 +0200

    minor

--- src/main/java/com/middle/htmlparser/Media/GetLogo.java
+++ src/main/java/com/middle/htmlparser/Media/GetLogo.java
@@ -13,10 +13,8 @@ import java.util.Scanner;
 public class GetLogo implements GetImg{
     @Override
     public String search(PrivateSpy spy)  throws IOException, JSONException{
-        String domain = spy.getDomain().getDomainName();
         String API_KEY = "2i9AEGZOrYE4I6cCGhLG8erzAZIqlfZbtm8zDBxL4Ok=";
-        domain = "lnu.edu.ua";
-        String query = URLEncoder.encode(domain, StandardCharsets.UTF_8);
+        String query = URLEncoder.encode(spy.getDomainName(), StandardCharsets.UTF_8);
         URL url = new URL("https://api.brandfetch.io/v2/brands/" + query);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
