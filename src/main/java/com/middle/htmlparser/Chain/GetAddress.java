9d955b0e71db183d182d27e4176e709935c36cc9
Olli <91615532+linndfors@users.noreply.github.com>
2022-12-03 16:45:28 +0200

    Update GetAddress.java

--- src/main/java/com/middle/htmlparser/Chain/GetAddress.java
+++ src/main/java/com/middle/htmlparser/Chain/GetAddress.java
@@ -30,26 +30,28 @@ public class GetAddress implements Chain{
                 address = match.group(0);
                 spy.setAddress(address);
             }
+            else {
+                try {
+                    String API_KEY = "5610ffe7442d63a7679402fb8245f1ad0dfb481f0eb89ce127ef1c9bc3be3d75";
+                    String url_query = "SELECT NAME FROM COMPANY WHERE WEBSITE='" + spy.getDomainName()+ "'";
+                    String query = URLEncoder.encode(url_query, StandardCharsets.UTF_8);
+                    URL url = new URL("https://api.peopledatalabs.com/v5/company/search?sql=" + query);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setRequestProperty("X-Api-Key", API_KEY);
+                    connection.connect();
+                    String text = new Scanner(connection.getInputStream()).useDelimiter("\\Z").next();
+                    JSONObject jsonObject = new JSONObject(text);
+                    String whole_address = jsonObject.getJSONArray("data").getJSONObject(0).getString("location");
+                    spy.setAddress(whole_address);
+                }
+                catch (Exception exception) {
+                    spy.setAddress(address);
+                }
+            }
         }
-
         catch (Exception ex) {
-            try {
-                String API_KEY = "5610ffe7442d63a7679402fb8245f1ad0dfb481f0eb89ce127ef1c9bc3be3d75";
-                String url_query = "SELECT NAME FROM COMPANY WHERE WEBSITE='" + spy.getDomainName()+ "'";
-                String query = URLEncoder.encode(url_query, StandardCharsets.UTF_8);
-                URL url = new URL("https://api.peopledatalabs.com/v5/company/search?sql=" + query);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("X-Api-Key", API_KEY);
-                connection.connect();
-                String text = new Scanner(connection.getInputStream()).useDelimiter("\\Z").next();
-                JSONObject jsonObject = new JSONObject(text);
-                String whole_address = jsonObject.getJSONArray("data").getJSONObject(0).getString("location");
-                spy.setAddress(whole_address);
-            }
-            catch (Exception exception) {
-                spy.setAddress(address);
-            }
+            spy.setAddress(address);
         }
     }
 }
